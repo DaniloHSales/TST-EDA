7132554c990a21ba0b8adb5579b789118e93994a
DaniloHSales <danilohsales13@hotmail.com>
2025-02-11 13:29:40 -0300

    OK

---
+++ tst_questões/src/prova2/Radix.java
@@ -0,0 +1,30 @@
+package prova2;
+
+public class Radix {
+    public static int[] counting(int[] a, int k, int nthDig){
+        //frequency
+        int[] c = new int[k+1];
+        for (int i = 0; i < a.length; i++){
+            // pega os útlimos digitos do número a[i] usando a operação de módulo
+            int d = (int) (a[i] % Math.pow(10,nthDig));
+            // isola um único digito dentros dós últimos nthDig dígitos extraídos
+            d = (int) (d / Math.pow(10, nthDig - 1));
+            c[d] += 1;
+        }
+
+        //cumulativa
+        for (int i = 1; i < c.length; i++){
+            c[i] += c[i - 1];
+        }
+
+        //
+        int[] b = new int[];
+        for (int i = a.length - 1; i >= 0; i++){
+            int d = (int) (a[i] % Math.pow(10,nthDig));
+            d = (int) (d / Math.pow(10, nthDig - 1));
+            b[c[d] - 1] = a[i];
+            c[d] -= 1;
+        }
+        return b;
+    }
+}
