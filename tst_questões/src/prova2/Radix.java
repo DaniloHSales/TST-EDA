43a3bdb4d4a8f54ba9e2d31db68eeec9462a7198
DaniloHSales <danilohsales13@hotmail.com>
2025-02-11 13:38:09 -0300

    OK

--- tst_questões/src/prova2/Radix.java
+++ tst_questões/src/prova2/Radix.java
@@ -1,30 +1,34 @@
 package prova2;
 
 public class Radix {
-    public static int[] counting(int[] a, int k, int nthDig){
-        //frequency
-        int[] c = new int[k+1];
-        for (int i = 0; i < a.length; i++){
-            // pega os útlimos digitos do número a[i] usando a operação de módulo
-            int d = (int) (a[i] % Math.pow(10,nthDig));
-            // isola um único digito dentros dós últimos nthDig dígitos extraídos
-            d = (int) (d / Math.pow(10, nthDig - 1));
-            c[d] += 1;
-        }
+    public static voi main(String[] args) {
 
-        //cumulativa
-        for (int i = 1; i < c.length; i++){
-            c[i] += c[i - 1];
-        }
 
-        //
-        int[] b = new int[a.length];
-        for (int i = a.length - 1; i >= 0; i++){
-            int d = (int) (a[i] % Math.pow(10,nthDig));
-            d = (int) (d / Math.pow(10, nthDig - 1));
-            b[c[d] - 1] = a[i];
-            c[d] -= 1;
+        public static int[] counting (int[] a, int k, int nthDig){
+            //frequency
+            int[] c = new int[k + 1];
+            for (int j : a) {
+                // pega os útlimos digitos do número a[i] usando a operação de módulo
+                int d = (int) (j % Math.pow(10, nthDig));
+                // isola um único digito dentros dós últimos nthDig dígitos extraídos
+                d = (int) (d / Math.pow(10, nthDig - 1));
+                c[d] += 1;
+            }
+
+            //cumulativa
+            for (int i = 1; i < c.length; i++) {
+                c[i] += c[i - 1];
+            }
+
+            //
+            int[] b = new int[a.length];
+            for (int i = a.length - 1; i >= 0; i++) {
+                int d = (int) (a[i] % Math.pow(10, nthDig));
+                d = (int) (d / Math.pow(10, nthDig - 1));
+                b[c[d] - 1] = a[i];
+                c[d] -= 1;
+            }
+            return b;
         }
-        return b;
     }
 }
